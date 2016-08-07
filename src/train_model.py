f1a38284b6ae8bc8d8e42544820b1c447602dff8
Mark Trovinger <mark.trovinger@gmail.com>
2016-08-07 15:39:07 -0400

    Added model code
    Added model code, plus code for model persistence.

--- src/train_model.py
+++ src/train_model.py
@@ -4,12 +4,12 @@
 ## 	
 import pandas as pd 
 import feather
-from tpot import TPOT
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OneHotEncoder
 from sklearn.cross_validation import train_test_split
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.externals import joblib
 
 path_to_processed_data = '../../data/processed/'
+path_to_model ='../models'
 
 df = feather.read_dataframe(path_to_processed_data + 'clean_dataset.feather')
 
@@ -23,6 +23,11 @@ df['PlayType'] = df['PlayType'].map({'Run' : 0, 'Pass': 1})
 # Split the dataset into seperate test and train sets
 (train_X, test_X, train_y, test_y) = train_test_split(df[features], df[target], test_size = 0.2)
 
-tpot = TPOT(generations=5)
+# Create the GradientBoostingClassifier, set with the weights from TPOT, and then fit the model to the 
+# dataset that we have
+gbc = GradientBoostingClassifier(learning_rate=0.16, max_features=1.0, 
+								 min_weight_fraction_leaf=1e-06, n_estimators=500, random_state=42)
+gbc.fit(train_X, train_y)
 
-tpot.fit(train_X, train_y)
+# Save the model for later use
+joblib.dump(gbc, path_to_model + 'gbc.pkl')
