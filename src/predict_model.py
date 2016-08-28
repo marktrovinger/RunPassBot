ffc61dc767ee9436818e06ef57cdd6183df614fb
Mark Trovinger <mark.trovinger@gmail.com>
2016-08-28 10:30:43 -0400

    Added data sets
    Added data sets to repo for the purpose of making it easier to mess
    with. Started adding code for model generation and training.

--- src/predict_model.py
+++ src/predict_model.py
@@ -7,6 +7,17 @@ from sklearn.externals import joblib
 path_to_model = '../models/'
 
 ## 
+# predict_playtype - Returns an array containing the predicted playtype (Run: 0, Pass: 1)
+#
+##
 def predict_playtype(test_X):
 	gbc = joblib.load(path_to_model + 'gbc.pkl')
-	gbc.predict(test_X)
+	return gbc.predict(test_X)
+
+##
+# predict_playtype_proba - Returns an array containing the probabilities that a play will be a Run or Pass.
+#
+##
+def predict_playtype_proba(test_X):
+	gbc = joblib.load(path_to_model + 'gbc.pkl')
+	return gbc.predict_proba(test_X)
---
+++ src/tpot_generation.py
@@ -0,0 +1,16 @@
+##
+# tpot_generation - Adds functionality for generating models using TPOT
+#
+##
+
+from tpot import TPOT
+
+##
+# generate_model - Uses TPOT to generate a probable model for usage, exports the
+#					model as a .py file
+##
+def generate_model(generations, verbosity, train_X, train_y):
+	tpot_generator = TPOT(generations=generations, verbosity=verbosity)
+	tpot_generator.fit(train_X, train_y)
+
+
