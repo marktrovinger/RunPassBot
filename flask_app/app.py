36a874b7cb47d369f25366f86e42e5e86c853e3c
Mark Trovinger <mark.trovinger@gmail.com>
2016-11-28 13:20:14 -0500

    Flask stuff
    Began flask implementation.

---
+++ flask_app/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+from wtforms import Form, TextAreaField, validators, IntegerField
+from sklearn.externals import joblib
+import pandas as pd
+
+
+app = Flask(__name__)
+
+###### Preparing classifier
+model_dir = '.../models/gbc.pkl'
+gbc_clf = joblib.load(model_dir)
+
+def classify(dataframe):
+	pass
+
+
+
+####### Flask
+class RunPassBot(Form):
+	
+
+
+@app.route('/')
+def index():
+	pass
+	return render_template('runpassform.html', form=form)
+
+@app.route('/prediction')
+def results():
+	pass
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
